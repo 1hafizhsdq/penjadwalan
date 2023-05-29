37084e6f83dd06c25656ba343a220fb5a7c83821
Hafizh Assidiq <hafizhassidiq77@gmail.com>
2023-05-29 14:09:59 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
 
 Auth::routes();
